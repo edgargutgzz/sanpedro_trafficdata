9c94a1614e02c3879ca0a5d3cd3aa21d071b7bc6
Edgar Gutierrez <edgargutierrezgzz@gmail.com>
2018-03-15 15:08:16 -0600

    Update routes on traffic.py.

--- traffic.py
+++ traffic.py
@@ -19,125 +19,221 @@ now = datetime.now()
 
 # Insert traffic data.
 
+
+# Atirantado desde Leones - Entrada
+
+response = gmaps.distance_matrix(('25.704677, -100.36991499999999'),('25.665602, -100.380839'),
+                                 departure_time = now)
+
+route_name = 'atirantado_leones_entrada'
+date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
+
+cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
+
+# Atirantado desde Leones - Salida
+
+response = gmaps.distance_matrix(('25.665587, -100.380774'),('25.704632999999998, -100.369858'),
+                                 departure_time = now)
+
+route_name = 'atirantado_leones_salida'
+date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
+
+cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
+
+# Atirantado desde Periferico - Entrada
+
+response = gmaps.distance_matrix(('25.696251, -100.38030099999999'),('25.665605, -100.380838'),
+                                 departure_time = now)
+
+route_name = 'atirantado_periferico_entrada'
+date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
+
+cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
+
+# Atirantado desde Periferico - Salida
+
+response = gmaps.distance_matrix(('25.665591, -100.380774'),('25.696168, -100.380245'),
+                                 departure_time = now)
+
+route_name = 'atirantado_periferico_salida'
+date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
+
+cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
+
 # Calzada Entrada
 
-response = gmaps.distance_matrix(('25.673569, -100.367882'),('25.662387, -100.368835'),
+response = gmaps.distance_matrix(('25.673569, -100.367882'),('25.658122, -100.370288'),
                                  departure_time = now)
 
 route_name = 'calzada_entrada'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # Calzada Salida
 
-response = gmaps.distance_matrix(('25.662583, -100.368320'),('25.674073, -100.367404'),
+response = gmaps.distance_matrix(('25.658008, -100.369799'),('25.673565999999997, -100.367599'),
                                  departure_time = now)
 
 route_name = 'calzada_salida'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # La Diana - Entrada
 
-response = gmaps.distance_matrix(('25.694560, -100.351416'),('25.652983, -100.352807'),
+response = gmaps.distance_matrix(('25.714492, -100.35020899999999'),('25.670654, -100.353571'),
                                  departure_time = now)
 
 route_name = 'ladiana_entrada'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # La Diana - Salida
 
-response = gmaps.distance_matrix(('25.653755, -100.352757'),('25.694564, -100.351219'),
+response = gmaps.distance_matrix(('25.670606, -100.353427'),('25.714427999999998, -100.35011'),
                                  departure_time = now)
 
 route_name = 'ladiana_salida'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # Tunel - Entrada
 
-response = gmaps.distance_matrix(('25.684975, -100.330676'),('25.655845, -100.337746'),
+response = gmaps.distance_matrix(('25.68706, -100.330349'),('25.655851, -100.337745'),
                                  departure_time = now)
 
 route_name = 'tunel_entrada'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # Tunel - Salida
 
-response = gmaps.distance_matrix(('25.655696, -100.337337'),('25.684963, -100.330551'),
+response = gmaps.distance_matrix(('25.655701, -100.33733799999999'),('25.687103, -100.330219'),
                                  departure_time = now)
 
 route_name = 'tunel_salida'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # Lazaro - Entrada
 
-response = gmaps.distance_matrix(('25.614267, -100.274253'),('25.650744, -100.332098'),
+response = gmaps.distance_matrix(('25.614269999999998, -100.274222'),('25.653682999999997, -100.357254'),
                                  departure_time = now)
 
 route_name = 'lazaro_entrada'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                      duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
+
 
 # Lazaro - Salida
 
-response = gmaps.distance_matrix(('25.645605, -100.324049'),('25.613813, -100.273075'),
+response = gmaps.distance_matrix(('25.652925999999997, -100.357528'),('25.614036, -100.274339'),
                                  departure_time = now)
 
 route_name = 'lazaro_salida'
 date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-distance = response['rows'][0]['elements'][0]['distance']['value']
-duration_traffic = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-avg_delay = float(np.round((duration_traffic/60)/(distance/1000), decimals = 2))
+distance = np.round((response['rows'][0]['elements'][0]['distance']['value']) / 1000, decimals = 1)
+duration_traffic = np.round((response['rows'][0]['elements'][0]['duration_in_traffic']['value']) / 60,
+                            decimals = 1)
+velocity = np.round((distance / duration_traffic) * 60, decimals = 0)
+avg_delay = np.round(duration_traffic / distance, decimals = 2)
 
 cursor.execute('''INSERT INTO traffic(route_name, date_time, distance, duration_traffic,
-                avg_delay) VALUES(?, ?, ?, ?, ?)''', (route_name, date_time, distance,
-                                                   duration_traffic, avg_delay))
+                velocity, avg_delay) VALUES(?, ?, ?, ?, ?, ?)''', (route_name, date_time, distance,
+                                                      duration_traffic, velocity, avg_delay))
 
 conn.commit()
 conn.close()
